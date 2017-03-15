5330f3465bc087ef0a88c0241d16497ef5bf79e9
az <1457800393@qq.com>
2017-03-15 18:55:13 +0800

    1111

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,41 +16,41 @@ angular
 				url: '/login',
 				templateUrl: 'views/login.html'
 			});
-//			.state("business", {
-//				url: '/business',
-//				templateUrl: 'views/business.html'
-//			});
-//			.state("firstPage", {
-//				url: '/firstPage',
-//				templateUrl: 'views/firstPage.html'
-//			});
-//			.state("officebox", {
-//				url: '/officebox',
-//				templateUrl: 'views/officebox.html'
-//			});
-//			.state("personinfo", {
-//				url: '/personinfo',
-//				templateUrl: 'views/personinfo.html'
-//			});
-//			.state("resource", {
-//				url: '/resource',
-//				templateUrl: 'views/resource.html'
-//			});
-//			.state("salary", {
-//				url: '/salary',
-//				templateUrl: 'views/salary.html'
-//			});
-//			.state("styleModality", {
-//				url: '/styleModality',
-//				templateUrl: 'views/styleModality.html'
-//			});
-//			.state("tel", {
-//				url: '/tel',
-//				templateUrl: 'views/tel.html'
-//			});
-//			.state("vacate", {
-//				url: '/vacate',
-//				templateUrl: 'views/vacate.html'
-//			});
+			.state("business", {
+				url: '/business',
+				templateUrl: 'views/business.html'
+			});
+			.state("firstPage", {
+				url: '/firstPage',
+				templateUrl: 'views/firstPage.html'
+			});
+			.state("officebox", {
+				url: '/officebox',
+				templateUrl: 'views/officebox.html'
+			});
+			.state("personinfo", {
+				url: '/personinfo',
+				templateUrl: 'views/personinfo.html'
+			});
+			.state("resource", {
+				url: '/resource',
+				templateUrl: 'views/resource.html'
+			});
+			.state("salary", {
+				url: '/salary',
+				templateUrl: 'views/salary.html'
+			});
+			.state("styleModality", {
+				url: '/styleModality',
+				templateUrl: 'views/styleModality.html'
+			});
+			.state("tel", {
+				url: '/tel',
+				templateUrl: 'views/tel.html'
+			});
+			.state("vacate", {
+				url: '/vacate',
+				templateUrl: 'views/vacate.html'
+			});
 		$urlRouterProvider.otherwise('/login')
 	}]);
