fc076eff7e3770e9fbe36ccec77738f81783bf95
Summer <1536904509@qq.com>
2017-03-16 16:08:01 +0800

    1

--- app/scripts/controllers/tel.js
+++ app/scripts/controllers/tel.js
@@ -8,6 +8,12 @@
  * Controller of the trainingProjectsApp
  */
 angular.module('trainingProjectsApp')
-  .controller('telCtrl', function () {
-    
-  });
+  .controller('telCtrl', ["$scope",'$http',function ($scope) {
+   
+    $http({
+    	url:"http//47.88.16.225:1604/test",
+    	method:'get'
+    }).then(function(e){
+    	 $scope.data=1;
+    })
+  }]);
