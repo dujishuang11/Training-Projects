242fb0f2f3ad9b5452be6bd729989b7e9e5c853d
dudu <1161071902@qq.com>
2017-03-16 15:26:57 +0800

    djs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,5 +11,50 @@
 angular
   .module('trainingProjectsApp', [
     'ngAnimate',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+  	$stateProvider.state("login",{
+  		url: '/login',
+  		templateUrl: 'views/login.html'
+  	})
+  	$stateProvider.state("business",{
+  		url: '/business',
+  		templateUrl: 'views/business.html'
+  	})
+  	$stateProvider.state("firstPage",{
+  		url: '/firstPage',
+  		templateUrl: 'views/firstPage.html'
+  	})
+  	$stateProvider.state("officebox",{
+  		url: '/officebox',
+  		templateUrl: 'views/officebox.html'
+  	})
+  	$stateProvider.state("personinfo",{
+  		url: '/personinfo',
+  		templateUrl: 'views/personinfo.html'
+  	})
+  	$stateProvider.state("resource",{
+  		url: '/resource',
+  		templateUrl: 'views/resource.html'
+  	})
+  	$stateProvider.state("salary",{
+  		url: '/salary',
+  		templateUrl: 'views/salary.html'
+  	})
+  	$stateProvider.state("styleModality",{
+  		url: '/styleModality',
+  		templateUrl: 'views/styleModality.html'
+  	})
+  	$stateProvider.state("tel",{
+  		url: '/tel',
+  		templateUrl: 'views/tel.html'
+  	})
+  	$stateProvider.state("vacate",{
+  		url: '/vacate',
+  		templateUrl: 'views/vacate.html'
+  	})
+  	$urlRouterProvider.otherwise('/styleModality')
+  }])
+  ;
