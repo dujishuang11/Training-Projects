9bbc595b2cadc2776bd02f29bda2374ae0d72612
az <1457800393@qq.com>
2017-03-16 19:08:17 +0800

    121111

--- app/scripts/controllers/salary.js
+++ app/scripts/controllers/salary.js
@@ -9,6 +9,20 @@
  */
 angular.module('trainingProjectsApp')
   .controller('salaryCtrl', function () {
-  });
-
+  })
+.directive('zhjSetDiv',function(){	  
+				return function(s,e,a){
+					var off=true;
+					e.bind('touchstart',function(){						 
+						 if(off){
+						 	e.text('完成');
+						 	off=false;
+						 }else{
+						 	e.text('编辑');
+						 	off=true;
+						 }
+						 
+					})
+				}
+			})
   
