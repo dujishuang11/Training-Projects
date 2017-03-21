22b0afc8ea5582f0a550f4af3e3ad248620a71a1
dudu <1161071902@qq.com>
2017-03-21 19:45:59 +0800

    djs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ angular
   	})
   	$stateProvider.state("business",{
   		url: '/business',
-  		templateUrl: 'views/business.html'
+  		templateUrl: 'views/firstPage.html'
   	})
   	$stateProvider.state("firstPage",{
   		url: '/firstPage',
@@ -34,7 +34,7 @@ angular
   	})
   	$stateProvider.state("personinfo",{
   		url: '/personinfo',
-  		templateUrl: 'views/personinfo.html'
+  		templateUrl: 'views/firstPage.html'
   	})
   	$stateProvider.state("resource",{
   		url: '/resource',
@@ -68,6 +68,6 @@ angular
   		url: '/enroll',
   		templateUrl: 'views/enroll.html'
   	})
-  	$urlRouterProvider.otherwise('/salary')
+  	$urlRouterProvider.otherwise('/tel')
   }])
   ;
