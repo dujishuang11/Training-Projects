4c3e82d4d05316844bfac76e46e2e3c189f741ea
Summer <1536904509@qq.com>
2017-03-21 11:25:28 +0800

    1

--- app/scripts/controllers/business.js
+++ app/scripts/controllers/business.js
@@ -8,27 +8,83 @@
  * Controller of the trainingProjectsApp
  */
 angular.module('trainingProjectsApp')
-  .controller('businessCtrl',function($scope){
-  	$(".wyh_sw").css("background","#cccccc");
-  	$scope.togglea=function(){
-  		$(".wyh_sw").css("background","#cccccc");
-  		$(".wyh_fp").css("background","none");
-		$(".wyh_taskfp").css("display","none")
-		$(".wyh_content").css("display","block")
+ .controller('businessCtrl',["$scope","$http",function($scope,$http){
+	$http({
+		url:"http://"+ip+":401/shiwu",
+		method:"get"
+	}).then(function(e){
+		$scope.items=e.data;
+	})
+	
+	$scope.Paixu="date";  //排序
+	$scope.isHide=true; //tab切换
+	$scope.bool=true;  //颜色切换
+	$scope.bools=false;
+//判断内容是否为空
+  	$scope.wyh_dates = '';
+  	$scope.wyh_uid = '';
+  	$scope.wyh_cont = '';
+  	$scope.isShow=false;
+  	$scope.togglea=function(Paixu){
+  		$scope.isHide=true;
+  		$scope.bool=true;
+		$scope.bools=false;
+		$http({
+			url:"http://"+ip+":401/shiwu",
+			method:"get"
+		}).then(function(e){
+			$scope.items=e.data;
+		})
   	}
   	$scope.toggleb=function(){
-  		$(".wyh_fp").css("background","#cccccc");
-  		$(".wyh_sw").css("background","none");
-		$(".wyh_content").css("display","none")
-		$(".wyh_taskfp").css("display","block")
+//		if(sessionStorage.level==3){
+//			console.log(sessionStorage.level)
+  			$scope.isHide=false;
+	  		$scope.bool=false;
+	  		$scope.bools=true;
+//			alert(1)
+//		}else{
+  			
+//		}
+  		
   	}
-  })
-.directive("wyhNewb",function(){
-	return function(s,e,a){
-		var hml="<ul class='taskfp_cont'><li><input type='text' class='form-control' /></li><li><input type='text' class='form-control'  /></li><li><input type='text' class='form-control' /></li></ul>";
-		e.find('.newadds').on("touchstart",function(){
-			e.find(".allul2").append(hml)
-		})
-	}
-}) 	
+  	$scope.wyh_none=function(){
+  		$scope.isShow = !$scope.isShow;
+  		
+  	}
+  	//保存
+  	$scope.wyhSave=function(){
+  		console.log($scope.wyh_dates)
+  		console.log($scope.wyh_cont)
+  		console.log($scope.wyh_uid)
+		if($scope.wyh_dates=='' || $scope.wyh_cont=="" || $scope.wyh_uid==""){
+			$scope.isShow = !$scope.isShow;
+		}else{
+			$http({
+				url:"http://"+ip+":401/shiwu",
+				method:"post",
+				data:{"date":$scope.wyh_dates,"content":$scope.wyh_cont,"uid":$scope.wyh_uid}
+			}).then(function(e){
+				console.log(e)
+				$scope.wyh_dates=""; //清空
+				$scope.wyh_cont="";
+				$scope.wyh_uid="";
+			})
+		}
+  	}
+  
+  		/*$http({
+			url:"http://"+ip+":401/users/?uid",
+			method:"get",
+//			data:{"username":$scope.wyh_uid}
+//			data:{"date":$scope.wyh_dates,"content":$scope.wyh_cont,"uid":$scope.wyh_uid}
+		}).then(function(e){
+			console.log(e)
+		})*/
+//		$scope.arr = [ { name : '水果', children : ['草莓','香蕉','葡萄'] }, { name : '蔬菜' children : ['西红柿','茄子','黄瓜'] } ]
+  	
+  }])
+
+
+
 
